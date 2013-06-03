e94b1ebdff8909521f98d9370252a2bfc886717c
Svante Lidman <svante.lidman@gmail.com>
2013-06-03 14:46:04 +0200

    First version on GitHub.

---
+++ CompletionCounter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using HPMSdk;
+using Hansoft.ObjectWrapper;
+
+namespace Hansoft.Jean.Behavior.DeriveBehavior.Expressions
+{
+    public class CompletionCounter
+    {
+        public static string CompletionCount(Task current_task)
+        {
+            if (!current_task.HasChildren)
+                return "";
+            int totalCount = 0;
+            int completedCount = 0;
+            foreach (Task task in current_task.DeepChildren)
+            {
+                if (!task.HasChildren)
+                {
+                    totalCount += 1;
+                    if ((EHPMTaskStatus)task.Status.Value == EHPMTaskStatus.Completed)
+                        completedCount += 1;
+                }
+            }
+            return completedCount.ToString() + "/" + totalCount.ToString();
+        }
+    }
+}
