843cc9127196147e51d186be18de49f4aa42cd17
samuelathansoft <samuel@hansoft.com>
2014-05-05 10:17:35 +0200

    Added test case example expresions

---
+++ AggregatePassCounter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using HPMSdk;
+using Hansoft.ObjectWrapper;
+
+namespace Hansoft.Jean.Behavior.DeriveBehavior.Expressions
+{
+    public class AggregatePassCounter
+    {
+        public static string PassCount(Task current_task)
+        {
+            if (!current_task.HasChildren)
+                return "";
+            int passCount = 0;
+            foreach (Task task in current_task.DeepChildren)
+            {
+                if (!task.HasChildren)
+                {
+                    string lastRun = task.GetCustomColumnValue("Last Run").ToString();
+                    string lastPass = task.GetCustomColumnValue("Latest Pass").ToString();
+
+                    if (lastRun != "" && lastRun == lastPass)
+                        passCount += 1;
+                }
+            }
+            return passCount.ToString();
+        }
+    }
+}
---
+++ ChildCounter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using HPMSdk;
+using Hansoft.ObjectWrapper;
+
+namespace Hansoft.Jean.Behavior.DeriveBehavior.Expressions
+{
+    public class ChildrenCounter
+    {
+        public static string ChildCount(Task current_task)
+        {
+            if (!current_task.HasChildren)
+                return "";
+            int totalCount = 0;
+            foreach (Task task in current_task.DeepChildren)
+            {
+                if (!task.HasChildren)
+                {
+                    totalCount += 1;
+                }
+            }
+            return totalCount.ToString();
+        }
+    }
+}
